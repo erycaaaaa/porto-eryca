fae6d069de58254ada5ce158d0cdc8935065437b
erycaaaaa <eryca847@gmail.com>
2025-10-20 20:30:11 +0700

    update hero

--- src/components/sections/hero/Hero.tsx
+++ src/components/sections/hero/Hero.tsx
@@ -36,7 +36,7 @@ export default function Hero({ name, subtitle }: HeroProps) {
       </div>
 
       {/* GRADIENT OVERLAY KE BAGIAN BAWAH */}
-      <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-12 bg-gradient-to-t from-[#f5f4ef] to-transparent" />
+      {/* <div className="pointer-events-none absolute bottom-0 left-0 right-0 h-12 bg-gradient-to-t from-[#f5f4ef] to-transparent" /> */}
     </main>
   );
 }
