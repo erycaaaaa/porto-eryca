8810dafce042b5d6aa126a0cb82cb999d74cf2b5
erycaaaaa <eryca847@gmail.com>
2025-12-25 17:59:17 +0700

    implement core technical SEO setup
     add canonical, robots meta, sitemap, and robots.txt

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,11 +1,14 @@
 // src/app/sitemap.ts
 import { MetadataRoute } from "next";
 
+export const dynamic = "force-static";
+
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://portfolio-erycadhamma.vercel.app",
+      url: "https://portfolio-erycadhamma.vercel.app/",
       lastModified: new Date(),
     },
   ];
 }
+        
